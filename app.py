5ded7507e1e36b0d94fa18bc4ecb4e630ed67365
rosario riccio <32274470+rosario-riccio@users.noreply.github.com>
2019-09-29 19:03:17 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+from views import *
+from jinja2 import Environment
+
+#add method to JINJA2
+app.jinja_env.add_extension('jinja2.ext.do')
+
+if __name__ == '__main__':
+    app.run(port=5500,debug=True)
