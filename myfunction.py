fbc7a1f36a3fdd5481a27c6a1c03d4d2485c569e
rosario riccio <32274470+rosario-riccio@users.noreply.github.com>
2019-10-13 18:26:30 +0200

    Update myfunction.py

--- myfunction.py
+++ myfunction.py
@@ -2,6 +2,15 @@
 
 from manager import *
 import json
+from datetime import timedelta,datetime
+from netCDF4 import Dataset
+from shapely.geometry import Polygon,Point
+
+#insert your path where there are netCDF file
+localPath = "<insert your path where there are netCDF file>"
+
+url = "http://193.205.230.6:8080/opendap/hyrax/opendap/wrf5"
+
 
 class MyTools(object):
 
@@ -70,44 +79,146 @@ class MyTools(object):
         cursorListPointCluster = json.dumps(list1)
         return cursorListPointCluster
 
-    def getPolygon(self,date1,managedb):
+    #Manage Point in Polygon's algorithm-------------------------------------------------------
+
+    def validityCheckDB(self,managedb):
+        """This method allows:
+        A) to check if DB was actived
+        B) to check if labels existed
+        C) to check if polygons existed
+        D) to check if every polygon had its label
+        """
+        try:
+            result, cursorLabelCollection = managedb.listLabelDB()
+            if result == False:
+                print("Error: there aren't label")
+                return result
+            result1, cursorClassPolygon = managedb.groupByClassPolygonDBNoDate()
+            if result1 == False:
+                print("Error: there aren't polygon")
+                return result
+            for polygon in cursorClassPolygon:
+                # print(polygon)
+                if (polygon["_id"]["name"] == ""):
+                    print("----------------------------------------")
+                    print("Error: there are polygons without its label")
+                    print("list of polygon without its label")
+                    name = polygon["_id"]["name"]
+                    result3,cursorPolygon = managedb.getPolygonOnName(name)
+                    if result3 == False:
+                        return result3
+                    else:
+                        for pol in cursorPolygon:
+                            print("id", pol["_id"], "datetime", pol["properties"]["dateStr"])
+                        result4 = False
+                        return result4
+            return True
+        except Exception as e:
+            print("error DB:",str(e))
+            result5 = False
+            return result5
+
+    def getUrls(self,rStart,rEnd,yStart,mStart,dStart,hStart,yEnd,mEnd,dEnd,hEnd):
+        """This method callows to seek netCDF in local/online of specific range of time"""
+        urls = []
+        ncfiles = []
+        for r in range(rStart,rEnd+1):
+            r1 = "d0" + str(r)
+            sdate = datetime(yStart,mStart,dStart,hStart)
+            edate = datetime(yEnd,mEnd,dEnd,hEnd)
+            delta = int((edate-sdate).total_seconds()/3600)
+            for i in range(delta+1):
+                tempDate = sdate + timedelta(hours=i)
+                time1 = tempDate.strftime("%Y%m%dZ%H%M")
+                year = tempDate.strftime("%Y")
+                month = tempDate.strftime("%m")
+                day = tempDate.strftime("%d")
+                url1 = url + "/" + r1 + "/" + "archive" + "/" + year + "/" + month + "/" + day + "/" + "wrf5_" + r1 + "_" + time1 + ".nc"
+                fname = "wrf5_"+ r1 + "_" + time1 + ".nc"
+                print(url1)
+                print(fname)
+                #http://193.205.230.6:8080/opendap/hyrax/opendap/wrf5/d01/archive/2018/08/01/wrf5_d01_20180801Z0000.nc
+                #http://193.205.230.6:8080/opendap/hyrax/opendap/wrf5/d01/archive/2018/10/29/wrf5_d01_20181029Z0200.nc
+
+                localPath1 = localPath + "/" + r1
+                localPath2 = localPath1 + "/" + fname
+                print(localPath2)
+                if not os.path.isfile(localPath2):
+                    print("file non presente in locale: " + fname)
+                    try:
+                        nc = Dataset(url1)
+                        nc.close()
+                        urls.append(url1)
+                        print("file presente online: " + fname)
+                    except Exception as e:
+                        print("file non presente online")
+                else:
+                    print("file presente in locale: " + fname)
+                    ncfiles.append(localPath2)
+        return ncfiles,urls
+
+    def getPolygon(self, date1, managedb):
+        """this method gets the polygons of specific date"""
         polygons = []
         typePolygons = []
-        print("Richiesta Polygon in base a datetime:",date1)
         try:
-            cursorPolygon = managedb.getPolygonOnDate(date1)
-            for polygon in cursorPolygon:
-                coords = []
-                #print("polygon DB")
-                #print(polygon)
-                #print(polygon["properties"]["name"].upper())
-                #print(polygon["geometry"]["coordinates"])
-                #print("lunghezza",len(polygon["geometry"]["coordinates"][0]))
-                for i in range(0,len(polygon["geometry"]["coordinates"][0])):
-                    print("lng:",polygon["geometry"]["coordinates"][0][i][0],"lat:",polygon["geometry"]["coordinates"][0][i][1])
-                    coords.append(tuple((polygon["geometry"]["coordinates"][0][i][0],polygon["geometry"]["coordinates"][0][i][1])))
-                typePolygons.append(self.assignTypeStorm(polygon))
-                #print(typePolygons[-1])
-                #print("geometric polygon")
-                #print(coords)
-                poly = Polygon(coords)
-                #print(poly)
-                polygons.append(poly)
-            #print(len(polygons),len(typePolygons))
-            return polygons,typePolygons
+            count,cursorPolygon = managedb.getPolygonOnDate(date1)
+            if count > 0:
+                for polygon in cursorPolygon:
+                    coords = []
+                    print("Info of polygon:")
+                    print(polygon)
+                    # print(polygon["properties"]["name"].upper())
+                    # print(polygon["geometry"]["coordinates"])
+                    # print("lunghezza",len(polygon["geometry"]["coordinates"][0]))
+                    for i in range(0, len(polygon["geometry"]["coordinates"][0])):
+                        # print("lng:",polygon["geometry"]["coordinates"][0][i][0],"lat:",polygon["geometry"]["coordinates"][0][i][1])
+                        coords.append(tuple(
+                            (polygon["geometry"]["coordinates"][0][i][0], polygon["geometry"]["coordinates"][0][i][1])))
+                    result,labelId = self.assignTypeStorm(managedb, polygon)
+                    if result == False:
+                        polygons= None
+                        typePolygons = None
+                        return polygons, typePolygons
+                    else:
+                        typePolygons.append(labelId)
+                    print("class of polygon:", typePolygons[-1])
+                    # print("geometric polygon")
+                    # print(coords)
+                    poly = Polygon(coords)
+                    # print(poly)
+                    polygons.append(poly)
+                # print("Number of polygon saved on DB n.",len(polygons))
+                return polygons, typePolygons
+            else:
+                return polygons, typePolygons
         except Exception as e:
-            print("exception error DB",e)
-            return None,None
+            print("Error DB:", str(e))
+            polygons = None
+            typePolygons = None
+            return polygons, typePolygons
 
-    def assignTypeStorm(self,polygon):
-        name = polygon["properties"]["name"].upper()
-        if name == "MEDICANE":
-            return 1
-        if name == "STORM":
-            return 2
-        if name == "THUNDERSTORM":
-            return 3
-
-
+    def assignTypeStorm(self, managedb, polygon):
+        try:
+            result, cursorLabelCollection1 = managedb.listLabelDB()
+            if result == False:
+                print("Error: there aren't labels")
+                labelId = None
+                return result,labelId
+            name = polygon["properties"]["name"].upper()
+            for label in cursorLabelCollection1:
+                name1 = label["labelName"].upper()
+                # print(name,name1)
+                if (name == name1):
+                    # print("assignment",name,name1)
+                    labelId = label["labelId"]
+                    # print("type",labelId)
+                    result = True
+                    return result,labelId
+        except Exception as e:
+            print("exception", str(e))
+            result = False
+            labelId = None
+            return result, labelId
 
 mytools = MyTools()
